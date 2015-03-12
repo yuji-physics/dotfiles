1e932fe0e3b323bc651d13462f8e3ad873fa3cef
yuji-physics <shrimp1205.physics@gmail.com>
2015-03-12 16:51:12 +0900

    brewfile

--- brewfile.sh
+++ brewfile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BREWFILE_LINES=`cat brewfile`
-for LINE in BREWFILE_LINTES
+for LINE in BREWFILE_LINES
 do
   LINEHEAD=echo $LINE | cut -c 1-1
-  if [ LINEHEAD != "#"]; then
+  if [ LINEHEAD != "#" ]; then
     "brew $LINE"
   fi
 done
